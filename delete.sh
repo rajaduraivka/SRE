60672fed551d19ef2bef91721360fd194b200175
rajaduraivka <rajaduraivka@gmail.com>
2020-02-01 12:26:30 +0530

    3rd commit

--- delete.sh
+++ delete.sh
@@ -4,3 +4,6 @@
 
 rm -rf -d ~/Desktop/*.png
 rm -rf -d ~/Downloads/*.csv
+rm -rf -d ~/Downloads/*.pdf
+rm -rf -d ~/Downloads/*.xlsx
+rm -rf -d ~/Downloads/*.jpg
--- sample.sh
+++ sample.sh
@@ -3,4 +3,4 @@ echo "what is your name?"
 read name
 echo "How do you do, $name?"
 read remark
-echo "I am $remark too!"
+echo "I am $remark too da!"
